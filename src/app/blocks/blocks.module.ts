016df5cef18c8b67372541c1ab75efb4168f6d4f
ghiath.alshammaa.ext <ghiath.alshammaa.ext@riag-digital.com>
2021-12-08 20:55:01 +0100

    prevent coreModule & BlocksModule in more that AppModule importing

--- src/app/blocks/blocks.module.ts
+++ src/app/blocks/blocks.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from './components/layout/layout.module';
 
@@ -7,4 +7,12 @@ import { LayoutModule } from './components/layout/layout.module';
   imports: [CommonModule, LayoutModule],
   exports: [LayoutModule],
 })
-export class BlocksModule {}
+export class BlocksModule {
+  constructor(@Optional() @SkipSelf() parentModule: BlocksModule) {
+    if (parentModule) {
+      throw Error(
+        'BlocksModule is already loaded. Import it in the AppModule only!'
+      );
+    }
+  }
+}
